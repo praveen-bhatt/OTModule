f2a4efe329274dc1b67cdeef6618f1ce82dba5ce
prem <singh.prem@vinculumgroup.com>
2016-06-16 13:56:56 +0530

    Commit ConfigSettings.Designer file

---
+++ OTAutomation/ConfigSettings.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OTAutomation
+{
+    partial class ConfigSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.gbConfig = new System.Windows.Forms.GroupBox();
+            this.pnlConfig = new System.Windows.Forms.Panel();
+            this.gbConfig.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(454, 316);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(373, 316);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // gbConfig
+            // 
+            this.gbConfig.Controls.Add(this.pnlConfig);
+            this.gbConfig.Location = new System.Drawing.Point(61, 29);
+            this.gbConfig.Name = "gbConfig";
+            this.gbConfig.Size = new System.Drawing.Size(481, 271);
+            this.gbConfig.TabIndex = 4;
+            this.gbConfig.TabStop = false;
+            this.gbConfig.Text = "Configuration";
+            // 
+            // pnlConfig
+            // 
+            this.pnlConfig.Location = new System.Drawing.Point(0, 19);
+            this.pnlConfig.Name = "pnlConfig";
+            this.pnlConfig.Size = new System.Drawing.Size(475, 246);
+            this.pnlConfig.TabIndex = 0;
+            // 
+            // ConfigSettings
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(618, 351);
+            this.ControlBox = false;
+            this.Controls.Add(this.gbConfig);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ConfigSettings";
+            this.Text = "ConfigSettings";
+            this.Load += new System.EventHandler(this.ConfigSettings_Load);
+            this.gbConfig.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox gbConfig;
+        private System.Windows.Forms.Panel pnlConfig;
+    }
+}
